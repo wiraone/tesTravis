a7026f584f46652a708da7319220bd8338105424
wiraone <xnamine@gmail.com>
2015-12-11 10:07:43 +0700

    refactor build code

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-xctool -workspace iPad.xcworkspace -scheme iPad -sdk iphoneos9.1 -configuration\
-  Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO 'CODE_SIGN_RESOURCE_RULES_PATH=$(SDKROOT)/ResourceRules.plist'}
+xctool -workspace iPad.xcworkspace -scheme iPad -sdk iphoneos9.1 -configuration "Release" CONFIGURATION_BUILD_DIR='~/build/' build
