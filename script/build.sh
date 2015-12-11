bfe8065203edf81e8901022a5ad911181cac2589
wiraone <xnamine@gmail.com>
2015-12-11 10:26:05 +0700

    change provisioning file

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
-xctool -workspace iPad.xcworkspace -scheme iPad -sdk iphoneos9.1 -configuration "Release" CONFIGURATION_BUILD_DIR='~/build/' build
+xctool -workspace iPad.xcworkspace -scheme iPad -sdk iphoneos9.1 -configuration\
+  Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
