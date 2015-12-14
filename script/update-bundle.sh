39341c8e1171c85692d428469508529cb4252d37
wiraone <xnamine@gmail.com>
2015-12-14 10:51:13 +0700

    tes build

--- script/update-bundle.sh
+++ script/update-bundle.sh
@@ -1,7 +1,7 @@
-if [ ! -z "$INFO_PLIST" ]; then
-  /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUNDLE_VERSION" "$INFO_PLIST"
-  echo "Set CFBundleVersion to $BUNDLE_VERSION"
-fi
+#if [ ! -z "$INFO_PLIST" ]; then
+#  /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $TRAVIS_BUILD_NUMBER" "$INFO_PLIST"
+#  echo "Set CFBundleVersion to $TRAVIS_BUILD_NUMBER"
+#fi
 
 if [ ! -z "$BUNDLE_IDENTIFIER" ]; then
   /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $BUNDLE_IDENTIFIER" "$INFO_PLIST"
