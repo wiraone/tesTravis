e2dc09c409f18805f26c1d2c574a1888038ba926
wiraone <xnamine@gmail.com>
2015-12-23 14:42:05 +0700

    fix build sh

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xcodebuild -workspace iPad.xcworkspace -scheme iPad -sdk iphoneos9.1 CODE_SIGN_RESOURCE_RULES_PATH='$(SDKROOT)/ResourceRules.plist' -configuration\
-  Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
+xcodebuild -workspace iPad.xcworkspace -scheme iPad -sdk iphoneos9.1 CODE_SIGN_RESOURCE_RULES_PATH='$(SDKROOT)/ResourceRules.plist' -configuration\ Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO | grep -A 5 error:
