fbcc6f11351291a404997cfc79d7a50976dc0898
wiraone <xnamine@gmail.com>
2015-12-23 13:27:58 +0700

    tes xcodebuild

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xctool -workspace iPad.xcworkspace -scheme iPad -sdk iphoneos9.1 -configuration\
+xcodebuild -workspace iPad.xcworkspace -scheme iPad -sdk iphoneos9.1 -configuration\
   Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
