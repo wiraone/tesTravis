9fd271cc84638daa88c225f649ef9390bcbde57b
wiraone <xnamine@gmail.com>
2015-12-23 19:25:29 +0700

    change build sh

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xcodebuild -workspace iPad.xcworkspace -scheme iPad -sdk iphoneos9.1 CODE_SIGN_RESOURCE_RULES_PATH='$(SDKROOT)/ResourceRules.plist' -configuration\ Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO > build_ios.log
+xcodebuild -workspace iPad.xcworkspace -scheme iPad -sdk iphoneos9.1 CODE_SIGN_RESOURCE_RULES_PATH='$(SDKROOT)/ResourceRules.plist' -configuration\
+  Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO > build_ios.log
